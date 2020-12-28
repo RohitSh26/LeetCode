d508779dbc12412bbb54a5653a2e81ed7b7ad7f3
Rohit Sharma <rohit.sharma1048@gmail.com>
2020-12-27 20:05:58 -0500

    complete Leetcode Valid Mountain Array Problem

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -9,31 +9,52 @@ namespace c_sharp
     {
         static void Main(string[] args)
         {
-            // Basics
-            System.Console.WriteLine("Basic Programming");
+            bool current = false;
 
-            // Fibonacci Recursive and Iterative
-            System.Console.WriteLine("Fibonacci Recursive and Iterative");
-            System.Console.WriteLine(Fibonacci.FibRecursive(n: 5));
-            System.Console.WriteLine(Fibonacci.FibIterative(n: 5));
+            if (!current)
+            {
+                // Basics
+                System.Console.WriteLine("Basic Programming");
 
-            System.Console.WriteLine();
+                // Fibonacci Recursive and Iterative
+                System.Console.WriteLine();
+                System.Console.WriteLine("Fibonacci Recursive and Iterative");
+                System.Console.WriteLine(Fibonacci.FibRecursive(n: 5));
+                System.Console.WriteLine(Fibonacci.FibIterative(n: 5));
 
-            // Leetcode Arrays
-            System.Console.WriteLine("CheckIfNumberAndItsDoubleExists");
-            System.Console.WriteLine(CheckIfNumberAndItsDoubleExists.CheckIfExist(arr: new int[] {10,2,5,3}));
-            System.Console.WriteLine(CheckIfNumberAndItsDoubleExists.CheckIfExist(arr: new int[] {3,1,7,11}));     
-            System.Console.WriteLine();
+                System.Console.WriteLine();
 
-            // Binary Search
-            System.Console.WriteLine("BinarySearchAlgorithm");
-            System.Console.WriteLine(BinarySearch.BinarySearchAlgorithm(nums: new int[] {-1,0,3,5,9,12}, target: 0));
+                // Leetcode Arrays
+                System.Console.WriteLine();
+                System.Console.WriteLine("CheckIfNumberAndItsDoubleExists");
+                System.Console.WriteLine(CheckIfNumberAndItsDoubleExists.CheckIfExist(arr: new int[] {10,2,5,3}));
+                System.Console.WriteLine(CheckIfNumberAndItsDoubleExists.CheckIfExist(arr: new int[] {3,1,7,11}));     
+                System.Console.WriteLine();
 
-            // Leet code Binary Search - SquareRoot
-            System.Console.WriteLine("SquareRoot");
-            System.Console.WriteLine(SquareRootProblem.SquareRoot(x: 19));
+                System.Console.WriteLine("ValidMountainArrayProblem");
+                System.Console.WriteLine(ValidMountainArrayProblem.ValidMountainArray(arr: new int[]{1,3, 2, 1, 0}));
+                System.Console.WriteLine(ValidMountainArrayProblem.ValidMountainArray(arr: new int[]{1,3, 2, 5, 1}));
 
-            System.Console.WriteLine();
+
+                // Binary Search
+                System.Console.WriteLine();
+                System.Console.WriteLine("BinarySearchAlgorithm");
+                System.Console.WriteLine(BinarySearch.BinarySearchAlgorithm(nums: new int[] {-1,0,3,5,9,12}, target: 0));
+
+                // Leet code Binary Search - SquareRoot
+                System.Console.WriteLine();
+                System.Console.WriteLine("SquareRoot");
+                System.Console.WriteLine(SquareRootProblem.SquareRoot(x: 19));
+
+                System.Console.WriteLine();
+            }
+            
+            // currently testing 
+
+            
+            
+
+
         }
     }
 }
