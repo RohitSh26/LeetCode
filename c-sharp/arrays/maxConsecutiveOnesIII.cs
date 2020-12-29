7601942fbefd61ea3cc408745136128cf5cfeec8
Rohit Sharma <rohit.sharma1048@gmail.com>
2020-12-29 15:54:56 -0500

    add leetcode  MaxConsecutiveOnesIII

---
+++ c-sharp/arrays/maxConsecutiveOnesIII.cs
@@ -0,0 +1,12 @@
+
+
+namespace c_sharp.arrays
+{
+    public class MaxConsecutiveOnesIII
+    {
+        public static int MaximumConsecutive()
+        {
+            return 0;
+        }
+    }
+}
