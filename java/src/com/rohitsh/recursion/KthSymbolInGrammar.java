492fb2389755b379246d69ececfd2e22fcd313e5
Rohit Sharma <rohit.sharma1048@gmail.com>
2021-01-02 22:16:41 -0500

    add leetcode recursive K-th Symbol in Grammar

---
+++ java/src/com/rohitsh/recursion/KthSymbolInGrammar.java
@@ -0,0 +1,22 @@
+package com.rohitsh.recursion;
+
+public class KthSymbolInGrammar {
+
+    public static int kthGrammar(int N, int K) {
+
+        
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(kthGrammar(1, 1));
+
+        System.out.println(kthGrammar(2, 1));
+
+        System.out.println(kthGrammar(2, 2));
+
+        System.out.println(kthGrammar(4, 5));
+    }
+}
